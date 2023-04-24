1f3190b675c869447295e5445dece7621c8159be
Philskillz <76884393+philskillz-coder@users.noreply.github.com>
2023-04-24 17:24:32 +0200

    updates

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
+function calcTime(offset) {
+  // create Date object for current location
+  var d = new Date();
+
+  // convert to msec
+  // subtract local time zone offset
+  // get UTC time in msec
+  var utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+
+  // create new Date object for different city
+  // using supplied offset
+  var nd = new Date(utc + (3600000*offset));
+
+  // return time as a string
+  return nd;
+}
+
+alert(calcTime('Bombay', '+5.5'));
 window.addEventListener("load", () => {
   clock();
   function clock() {
-    const today = new Date();
+    const today = calcTime(2);
 
     // get time components
     const hours = today.getHours();
